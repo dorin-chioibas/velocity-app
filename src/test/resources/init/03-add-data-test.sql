4da1a5cad95dea8b2c59018e6920a8511e458f66
Dorin Chioibas <dorin91dorin@gmail.com>
2024-11-01 16:53:21 +0200

    Add PostgreSQL DDL and DML scripts for tests

---
+++ src/test/resources/init/03-add-data-test.sql
@@ -0,0 +1,19 @@
+INSERT INTO Bird (name, color, weight, height)
+VALUES ('Vrabie', 'Maro', 500, 10);
+INSERT INTO Bird (name, color, weight, height)
+VALUES ('Cocosar', 'Rosu', 2000, 20);
+INSERT INTO Bird (name, color, weight, height)
+VALUES ('Gaita', 'Albastru', 1000, 35);
+
+INSERT INTO Sighting (bird_id, location, date_time)
+VALUES (1, 'Delta Dunarii', '2008-06-01 08:00:00');
+INSERT INTO Sighting (bird_id, location, date_time)
+VALUES (1, 'Delta Dunarii', '2009-06-01 08:00:00');
+INSERT INTO Sighting (bird_id, location, date_time)
+VALUES (1, 'Delta Dunarii', '2022-06-01 08:00:00');
+INSERT INTO Sighting (bird_id, location, date_time)
+VALUES (1, 'Parcul Cismigiu', '2012-07-05 14:00:00');
+INSERT INTO Sighting (bird_id, location, date_time)
+VALUES (2, 'Parcul Herastrau', '2011-07-15 09:30:00');
+INSERT INTO Sighting (bird_id, location, date_time)
+VALUES (3, 'Muntii Carpati', '2024-08-20 11:00:00');
